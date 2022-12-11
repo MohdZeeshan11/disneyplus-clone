bb63772e91d0f183e9d2b765ae67be8f0abd2fc4
zeeshan@truecounsel <mohdzeeshan7775@gmail.com>
2022-12-11 18:29:40 +0530

    error fixed

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/storage";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJBytruORO3925SpWiNrsn0D2AamFPTuI",
-  authDomain: "disneyplus-clone-c7949.firebaseapp.com",
-  projectId: "disneyplus-clone-c7949",
-  storageBucket: "disneyplus-clone-c7949.appspot.com",
-  messagingSenderId: "1083605783828",
-  appId: "1:1083605783828:web:d45638eb3a680487659e01",
+  apiKey: "AIzaSyAZXx1ZhPRF5hUddzXJ1nK_NjFmorx1XfA",
+  authDomain: "disneyplusclone-c5594.firebaseapp.com",
+  projectId: "disneyplusclone-c5594",
+  storageBucket: "disneyplusclone-c5594.appspot.com",
+  messagingSenderId: "63043894424",
+  appId: "1:63043894424:web:098855933cc0e4d62ea4ed"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
